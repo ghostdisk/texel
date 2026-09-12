df155869e0a3ddeb5adbb42ea8993855719c977e
Alex <alex@panzermail.com>
2026-09-12 23:32:45 +0300

    Group drawing tools and refine eyedropper icons

--- src/tools/cursors.ts
+++ src/tools/cursors.ts
@@ -2,4 +2,4 @@ import rotateUrl from '../assets/cursor-rotate.svg?url';
 import eyedropperUrl from '../assets/cursor-eyedropper.svg?url';
 
 export const ROTATE_CURSOR = `url("${rotateUrl}") 16 16, crosshair`;
-export const EYEDROPPER_CURSOR = `url("${eyedropperUrl}") 8 28, crosshair`;
+export const EYEDROPPER_CURSOR = `url("${eyedropperUrl}") 7 26, crosshair`;
